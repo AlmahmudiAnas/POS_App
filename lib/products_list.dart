6323bd6c80614b4509fc2f3e07e6dbf797427feb
Anas Almahmudi <94315840+AlmahmudiAnas@users.noreply.github.com>
2022-11-26 14:34:46 +0200

    testing product List

---
+++ lib/products_list.dart
@@ -0,0 +1,12 @@
+class Products {
+  String imageURL,productName,productGroupPrice,productPrice;
+  Products({required this.imageURL, required this.productName, required this.productGroupPrice, required this.productPrice})
+}
+
+List <Products> {
+  Products(imageURL:"",productName:"",productGroupPrice:"",productPrice:"",),
+  Products(imageURL:"",productName:"",productGroupPrice:"",productPrice:"",),
+  Products(imageURL:"",productName:"",productGroupPrice:"",productPrice:"",),
+  Products(imageURL:"",productName:"",productGroupPrice:"",productPrice:"",),
+  Products(imageURL:"",productName:"",productGroupPrice:"",productPrice:"",),
+}
