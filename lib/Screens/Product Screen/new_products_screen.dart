3dbcb4b1760fac61b217ab1d171b565b2c50bd44
Anas <anesmedia263@gmail.com>
2022-11-28 14:18:38 +0200

    New Product Screen

--- lib/Screens/Product Screen/new_products_screen.dart
+++ lib/Screens/Product Screen/new_products_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class NewProductScreen extends StatelessWidget {
-  const NewProductScreen({super.key});
+  NewProductScreen({super.key});
   static String routeName = "New Product Screen";
+  TextEditingController _productName = TextEditingController();
+  TextEditingController _productCode = TextEditingController();
+  TextEditingController _productCate = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("New Product"),
@@ -18,7 +22,35 @@ class NewProductScreen extends StatelessWidget {
                 radius: 50,
                 backgroundColor: Colors.grey,
               ),
-              
+              SizedBox(height: size.height * 0.02),
+              TextField(
+                controller: _productName,
+                decoration: InputDecoration(
+                  label: Text('Product Name'),
+                ),
+              ),
+              SizedBox(height: size.height * 0.02),
+              TextField(
+                controller: _productCate,
+                decoration: InputDecoration(
+                  label: Text('Category'),
+                ),
+              ),
+              SizedBox(height: size.height * 0.02),
+              Row(
+                children: [
+                  TextField(
+                    controller: _productCode,
+                    decoration: InputDecoration(
+                      label: Text('Product Code'),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.camera),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
